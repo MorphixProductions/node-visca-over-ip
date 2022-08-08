e58b394997370f28180898ec5ae6bc92ff420b5c
Mees van der Wijk <mees@vix-entertainment.nl>
2022-08-08 22:07:09 +0200

    Added main and completed package.json

---
+++ src/ViscaOverIP.ts
@@ -0,0 +1,7 @@
+// INSPIRED BY https://github.com/br8kpoint/node-visca
+//
+// Created by MorphixProductions - Mees van der Wijk <3
+
+export * from './camera';
+export * from './command';
+export * from './constants';
